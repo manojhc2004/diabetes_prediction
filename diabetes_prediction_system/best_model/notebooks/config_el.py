f4e54ae890996656ef47c38f6b07c42a48146940
manoj hc <manojhc.dev@gmail.com>
2026-07-17 21:22:30 +0530

    Create config_el.py

---
+++ diabetes_prediction_system/best_model/notebooks/config_el.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+
+#sk
+    # ensemble
+    
+# bagging
+from sklearn.ensemble import RandomForestClassifier
+
+
+
+# basic
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report,accuracy_score
