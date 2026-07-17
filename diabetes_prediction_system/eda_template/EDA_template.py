bcdabcd66c4e59408f302c1c31bad0775c65a4c8
manoj hc <manojhc.dev@gmail.com>
2026-07-17 20:57:06 +0530

    # eda temp

---
+++ diabetes_prediction_system/eda_template/EDA_template.py
@@ -0,0 +1,49 @@
+class Basics:
+    
+    # basic inspection of data
+    
+    # number of rows and columns
+    # data describe, column describe
+    # missing values
+    # condition based on 'Column'
+    # data-type
+    pass
+                            
+                                                     
+class Target_Analysis:
+    
+    # analysing particular target columns
+    
+    # it involves
+        """
+        1. number of nondiabetes patients
+        2. number of diabetes patients
+        3. distribution of diabetes based on age"""
+        pass
+            
+            
+class Univariate_Analysis:
+    
+    
+    # Analysis column vise 
+        # skewness
+        # column distribuion
+        # identifying the outliers
+        # specific column describe
+    pass
+        
+        
+
+class Bivariate_Analysis:
+    
+    # Analysis specific two columns relation-ship
+        # positive relation ship
+        # negative relation ship
+    pass
+
+class Multivariate_Analysis:
+    
+    # Analyzing the two or more columns with 'Outcome' or 'Output'
+        # identifying the effects of the columns which combination s raising the diabetes
+        # identifying the combination is effecinting or not!
+    pass
