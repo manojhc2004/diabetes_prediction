7b37358db862a97e234ce910bc14b29575f08f05
manoj hc <manojhc.dev@gmail.com>
2026-07-21 00:53:49 +0530

    # multivariate analysis

--- diabetes_prediction_system/multivariate_analysis.py
+++ diabetes_prediction_system/multivariate_analysis.py
@@ -1,4 +1,4 @@
-from diabetes_prediction_system.config import *
+from config import *
 
 class Multivariate_Analysis:
 
@@ -9,6 +9,29 @@ class Multivariate_Analysis:
     def pairplot(self,col1,col2,col3):
         
             plt.figure(figsize=(6,4))
-            plt.title(f'{col1} and {col2} realtion ship')
-            sns.scatterplot(x=col1,y=col2,hue=col3,data=self.df)
-            plt.show()
+            sns.boxplot(data=self.df,
+                        x=col1,
+                        y=col2,
+                        hue=col3)
+    
+    
+    def threerelation(self,col1,col2,col3):
+        
+        fig = plt.figure(figsize=(8,6))
+        ax = fig.add_subplot(111, projection='3d')
+       
+        ax.scatter(
+            self.df[col1],
+            self.df[col2],
+            self.df[col3],
+            c = self.df["Outcome"])
+
+        ax.set_xlabel(col1, labelpad=15)
+        ax.set_ylabel(col2, labelpad=15)
+        ax.set_zlabel(col3, labelpad=15)
+
+        #ax.view_init(elev=25, azim=35)
+
+        plt.title(f"3D Scatter Plot: {col1} vs {col2} vs {col3}")
+        plt.tight_layout()
+        plt.show()
