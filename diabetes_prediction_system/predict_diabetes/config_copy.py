2e02924580dfc8644a09323ffc4bd3094abf8dec
manoj hc <manojhc.dev@gmail.com>
2026-07-21 00:55:10 +0530

    Create config_copy.py

---
+++ diabetes_prediction_system/predict_diabetes/config_copy.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from mpl_toolkits.mplot3d import Axes3D
+
+#sklearn dependancies
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,classification_report, confusion_matrix
+
+#job
+import joblib
+
+# reload
+import importlib
