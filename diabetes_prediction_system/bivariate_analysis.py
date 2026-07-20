d96ef60fd1531e3725dee9247324d70ed73d5552
manoj hc <manojhc.dev@gmail.com>
2026-07-21 00:56:15 +0530

    # bivariate analysis

--- diabetes_prediction_system/bivariate_analysis.py
+++ diabetes_prediction_system/bivariate_analysis.py
@@ -1,13 +1,17 @@
-from diabetes_prediction_system.config import *
+from config import *
 
 class Bivariate_Analysis:
             
         def __init__(self,df):
             self.df = df
-        
-        def corr(self,col1,col2):
             
-            plt.figure(figsize=(5,3))
-            sns.histplot(data=self.df,x=col1,hue=col2,kde=True)
-            plt.title(f'{col1} and {col2} relation ship')
-            plt.show()
+        def corr_(self,col1,col2):
+            
+            plt.figure(figsize=(7,5))
+            sns.scatterplot(data=self.df,
+                            x=self.df[col1],
+                            y=self.df[col2],
+                            hue=self.df['Outcome']
+                            )
+            plt.title(f" {col1} Vs {col2} relation")
+        
